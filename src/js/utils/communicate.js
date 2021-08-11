b2c6002a5be7ab9dc9bd473d253106fcac09d675
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2021-08-11 16:25:10 +0430

    feat: send message from client to sw

--- src/js/utils/communicate.js
+++ src/js/utils/communicate.js
@@ -3,5 +3,18 @@
  * @param {*} data - message content
  */
 export function handleMessage({ data }) {
-  console.log(data)
+  console.log('[A Message From Service Worker]: ', data)
+}
+
+/**
+ * send message to service worker
+ * @param {*} msg 
+ */
+export function sendMessage(msg) {
+  try {
+    navigator.serviceWorker.controller.postMessage(msg)
+
+  } catch(e) {
+    console.error('[Send Message To Service Worker]: ', e)
+  }
 }
